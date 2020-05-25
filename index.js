2eb005bf4993e0debf567cd17a83eea02b5f31fa
Gonzalez32 <3232rose@gmail.com>
2020-05-25 17:05:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your code here
+// // Write your code here
+// class Dog {
+//     constructor(name, breed) {
+//         this.name = name;
+//         this.name = breed;
+//     }
+// }
+
+// let bigDog = new Dog('Buzz', 'greatPyrenees');
+// let samllDog = new Dog("Oreo", "Pit-bull");
+
+// samllDog()
+// bigDog()
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+Breakfast 
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+Lunch
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        // private property by including an underscore_ at the beginning
+        this._dessert = dessert;
+    }
+}
+
+Dinner
